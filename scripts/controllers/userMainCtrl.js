6c38f1cdbe8fcbe31caecb993c3f19fb38200073
Maksim <reckless04@mail.ru>
2016-03-11 17:57:07 +0300

    responsive table add

--- scripts/controllers/userMainCtrl.js
+++ scripts/controllers/userMainCtrl.js
@@ -1,13 +1,14 @@
 wogApp.controller('userMainCtrl', userMainCtrl);
 
 function userMainCtrl($rootScope, $scope) {
-  
+
   $rootScope.title = '/ Кабінет користувача';
   $rootScope.page_img = 'main';
   angular.element($('.nav_list li')).removeClass('active');
   angular.element($('.main')).addClass('active');
-  
-  $.getScript("scripts/jquery/test.js", function(){
-  alert("Script loaded and executed.");
-});
+
+  $.getScript("scripts/lib/footable.js", function () {
+    console.log("Script footable - ready");
+    $('.table').footable();
+  });
 }
