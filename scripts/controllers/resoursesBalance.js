c3bf2897d618bd5d169a53a7f6aea5cdd0ecea7e
Maksim <reckless04@mail.ru>
2016-03-21 16:14:28 +0300

    add title

---
+++ scripts/controllers/resoursesBalance.js
@@ -0,0 +1,46 @@
+wogApp.controller('resoursesBalance', function ($scope) {
+  $scope.fuel_types = [
+    {
+      name: 'mustang_95',
+      balance: '10000',
+      date: '21/12/2016',
+      text: ''
+    },
+    {
+      name: 'mustang_92',
+      balance: '10000',
+      date: '21/12/2016',
+      text: ''
+    },
+    {
+      name: 'mustang_100',
+      balance: '10000',
+      date: '21/12/2016',
+      text: ''
+    },
+    {
+      name: 'dp_mustang',
+      balance: '10000',
+      date: '21/12/2016',
+      text: ''
+    },
+    {
+      name: 'dp_mustang_plus',
+      balance: '10000',
+      date: '21/12/2016',
+      text: ''
+    },
+    {
+      name: 'lgp_mustang',
+      balance: '10000',
+      date: '21/12/2016',
+      text: ''
+    },
+    {
+      name: 'Гроші',
+      balance: '10000',
+      date: '21/12/2016',
+      text: 'Гроші'
+    }
+  ]
+});
