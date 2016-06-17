81e40381b97cdf7828eeb2006c13aa532b4b94d6
AntonShulzhenko <shulzhenkomailbox@gmail.com>
2016-06-17 16:26:50 +0300

    map

--- scripts/services/gmap.js
+++ scripts/services/gmap.js
@@ -6,6 +6,15 @@ function initMap() {
     center: {lat: 49.01, lng: 31.28},
     zoom: 6
   });
+  var places = [
+    {lat: 50.531307, lng: 30.227661},
+    {lat: 50.534901, lng: 30.776111},
+    {lat: 50.385690, lng: 30.892239},
+    {lat: 50.460310, lng: 29.807563},
+    {lat: 50.501158, lng: 29.212713},
+    {lat: 49.767457, lng: 24.424388},
+    {lat: 49.715752, lng: 23.243534}
+  ];
 
   // Marker
   var img = 'img/map-ico.png';
@@ -15,35 +24,12 @@ function initMap() {
   var destination_marker = new google.maps.Marker({
     icon: img
   });
+  var originMarker;
+  var destinationMarker;
+
   origin_marker.setMap(map);
   destination_marker.setMap(map);
   // /Marker
-
-  // Info Window
-  var infowindow = new google.maps.InfoWindow({
-    content: document.getElementById('info-window')
-    // content: document.getElementById('info-table')
-    // maxWidth: 300
-  });
-
-  origin_marker.addListener('click', function() {
-    infowindow.open(map, origin_marker);
-  });
-
-  destination_marker.addListener('click', function() {
-    infowindow.open(map, destination_marker);
-  });
-  // /Info Window
-
-  // Polyline
-  var poly = new google.maps.Polyline({
-    strokeColor: '#00ab4d',
-    strokeOpacity: 1.0,
-    strokeWeight: 3,
-    map: map,
-    fillColor: '#00ab4d'
-  });
-  // /Polyline
 
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
@@ -51,7 +37,6 @@ function initMap() {
 
   var origin_input = document.getElementById('input-start');
   var destination_input = document.getElementById('input-end');
-  var modes = document.getElementById('mode-selector');
 
   var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
   origin_autocomplete.bindTo('bounds', map);
@@ -60,24 +45,65 @@ function initMap() {
 
   origin_autocomplete.addListener('place_changed', function() {
     var place = origin_autocomplete.getPlace();
+    var placeLat = place.geometry.location.lat();
+    var placeLng = place.geometry.location.lng();
+
     if (!place.geometry) {
       window.alert("Autocomplete's returned place contains no geometry");
       return;
     }
-    origin_marker.setPosition(place.geometry.location);
+    // origin_marker.setPosition(place.geometry.location);
+    var windowArray = [];
+    for(var i = 0; i < places.length; i++) {
+      windowArray.push(infowindow);
 
+      var dist = distance(placeLat, placeLng, places[i]);
+
+      if(dist < 60) {
+        originMarker = new google.maps.Marker({
+          map: map,
+          icon: img,
+          position: new google.maps.LatLng(places[i].lat, places[i].lng)
+        });
+
+        originMarker.addListener('click', function() {
+          infowindow.open(map, originMarker);
+        });
+      }
+    }
+    console.log(windowArray);
     // If the place has a geometry, store its place ID and route if we have
     // the other place ID
     origin_place_id = place.place_id;
   });
 
+  var infowindow = new google.maps.InfoWindow({
+    content: document.getElementById('info-window')
+    // content: document.getElementById('info-table')
+    // maxWidth: 300
+  });
+
   destination_autocomplete.addListener('place_changed', function() {
     var place = destination_autocomplete.getPlace();
+    var placeLat = place.geometry.location.lat();
+    var placeLng = place.geometry.location.lng();
     if (!place.geometry) {
       window.alert("Autocomplete's returned place contains no geometry");
       return;
     }
-    destination_marker.setPosition(place.geometry.location);
+    // destination_marker.setPosition(place.geometry.location);
+
+    for(var i = 0; i < places.length; i++) {
+      var dist = distance(placeLat, placeLng, places[i]);
+
+      if(dist < 60) {
+        destinationMarker = new google.maps.Marker({
+          map: map,
+          icon: img,
+          position: new google.maps.LatLng(places[i].lat, places[i].lng)
+        });
+      }
+    }
 
     // If the place has a geometry, store its place ID and route if we have
     // the other place ID
@@ -103,5 +129,14 @@ function initMap() {
         window.alert('Directions request failed due to ' + status);
       }
     });
+  }
+
+  // Distance between original and destination
+  function distance(origLat, origLng, dest) {
+    var LatKmInOneDeg = 111;
+    var LngKmInOneDeg = 65;
+    var result = Math.sqrt(Math.pow(((dest.lat - origLat)*LatKmInOneDeg), 2) + Math.pow(((dest.lng - origLng)*LngKmInOneDeg), 2));
+    result = Math.round(result);
+    return result;
   }
 }
